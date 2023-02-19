ccf1283006143b970c7bf70f1518bdf46e2475b3
Imre Korf <I.korf@outlook.com>
2023-02-19 13:07:03 +0100

    Initial commit

---
+++ config_bits.h
@@ -0,0 +1,43 @@
+/* 
+ * File:   config_bits.h
+ * Author: mcimr
+ *
+ * Created on February 19, 2023, 12:30 PM
+ */
+
+#ifndef CONFIG_BITS_H
+#define	CONFIG_BITS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+// PIC12F1501 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1
+#pragma config FOSC = INTOSC    // Oscillator Selection Bits (INTOSC oscillator: I/O function on CLKIN pin)
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable (PWRT enabled)
+#pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LPBOR = ON       // Low-Power Brown Out Reset (Low-Power BOR is enabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CONFIG_BITS_H */
+
---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * File:   main.c
+ * Author: mcimr
+ *
+ * Created on February 19, 2023, 12:29 PM
+ */
+
+
+#include <xc.h>
+#include "config_bits.h"
+
+#pragma config IDLOC0 = 1, IDLOC1 = 4, IDLOC2 = 5, IDLOC3=9
+
+void main(void) {
+    
+    while(1) {
+        
+    }
+    
+    return;
+}
+
+
